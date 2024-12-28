5796589521c98ae6f7fb5a5619e293d8cd34c1a7
Niti <b321024@iiit-bh.ac.in>
2024-12-28 23:57:04 +0530

    Added product page

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    content: [
+        "./src/**/*.{js,jsx,ts,tsx}", // Include all your source files
+        "./public/index.html", // Include your HTML file if needed
+    ],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+};
